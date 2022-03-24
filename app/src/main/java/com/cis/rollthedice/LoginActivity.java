396ec166831578e18b0706421f85f31813e278dd
Sahan Thilina <sahanthilina14@gmail.com>
2022-03-24 22:28:14 +0530

    initial code

---
+++ app/src/main/java/com/cis/rollthedice/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.cis.rollthedice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        int no=32;
+    }
+}
